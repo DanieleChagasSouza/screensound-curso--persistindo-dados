1032d056175cf12177516aae6acfb4017b937a1c
Daniele Chagas <daniele.chagas@performait.com>
2024-05-23 15:46:13 -0300

    fix: Aplicando novos conceitos no projeto

---
+++ ScreenSound/Migrations/20240523135546_PopularTabela.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Artistas", 
+                new string[] {"Nome", "Bio", "FotoPerfil"},
+                new object[] {"Aline Barros", "É uma cantora, compositora, multi-instrumentista, escritora, empresária, pastora e apresentadora brasileira.",
+                    "https://akamai.sscdn.co/uploadfile/letras/fotos/c/5/7/0/c570bc6a39ef159ca86eab7868fe1d55-tb_140.jpg" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" },
+                new object[] { "Eyshila Oliveira", "É uma cantora, pastora, escritora e compositora brasileira de música gospel.",
+                    "https://akamai.sscdn.co/uploadfile/letras/fotos/1/4/f/e/14fee894c507846bf24ee5cac1a596db-tb_140.jpg" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" },
+                new object[] { "Damares Alves", "Mais conhecida apenas como Damares, é uma cantora e compositora brasileira de música cristã contemporânea ligada ao movimento pentecostal.",
+                "https://akamai.sscdn.co/uploadfile/letras/fotos/2/3/b/9/23b985739895483a6369b6ddf22b230b-tb_140.jpg"});
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio", "FotoPerfil" },
+                new object[] { "Anderson Ricardo Freire", "É um cantor e compositor brasileiro, conhecido como intérprete de música cristã contemporânea para uma série de artistas.",
+                    "https://akamai.sscdn.co/uploadfile/letras/fotos/a/4/2/7/a427f81f2a19e8beaf94d98b12559ded-tb_140.jpg"});
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Artistas");
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20240523161106_PopularMusicas.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularMusicas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento"}, new object[] { "Te Adorar é Só o Começo", "2024" });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Canção de Filho", "2023" });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Quem Te Guarda É Deus", "2023" });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "Cordeiro Que Venceu", "2023" });
+
+            migrationBuilder.InsertData("Musicas", new string[] { "Nome", "AnoLancamento" }, new object[] { "onda-me e Usa-me", "2023" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Musicas");
+        }
+    }
+}
